8de9782f3895783e3215314035fe0c25e02e4693
Brian Wu <sonny40920710@gmail.com>
2021-07-25 16:21:22 +0800

    implement backtracking algorithm in n_queens problem

--- 1_mathematical_thinking_in_computer_science/n_queens.py
+++ 1_mathematical_thinking_in_computer_science/n_queens.py
@@ -22,8 +22,32 @@ def brute_force_search(n: int) -> list:
             solutions.append(perm)
     return solutions
 
+# 2. Backtracking method
+def is_sub_solution(sub_sol: list) -> bool:
+    for i1, i2 in itertools.combinations(range(len(sub_sol)), 2):
+        if abs(i1 - i2) == abs(sub_sol[i1] - sub_sol[i2]):
+            return False
+    return True
+
+def extend(perm: list, n: int, solutions: list) -> None:
+    if len(perm) == n:
+        solutions.append(perm)
+    # extend the permutation
+    for i in range(n):
+        if i not in perm:
+            perm.append(i)
+            if is_sub_solution(perm):
+                extend(perm, n, solutions)
+            perm.pop()
+
+def back_track_search(n: int) -> list:
+    solutions = []
+    extend(perm=[], n=n, solutions=solutions)
+    return solutions
+
+
 if __name__ == "__main__":
-    n = 8
-    solutions = brute_force_search(n)
-    for sol in solutions:
-        print(sol)
+    max_n = 10
+    for i in range(1, max_n + 1):
+        solutions = back_track_search(i) # much faster than brute_force_search
+        print(f"The number of solutions of n={i}: {len(solutions)}")
