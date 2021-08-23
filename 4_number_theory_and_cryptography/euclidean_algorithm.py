a79e55e0a7fca3cebb8e23db0b360bc805e127e2
Brian Wu <sonny40920710@gmail.com>
2021-08-23 12:03:03 +0800

    implement extended Euclidean algorithm

--- 4_number_theory_and_cryptography/euclidean_algorithm.py
+++ 4_number_theory_and_cryptography/euclidean_algorithm.py
@@ -1,7 +1,20 @@
-def gcd(a, b):
+def gcd(a: int, b: int) -> int:
     assert a >= 0 and b >= 0
     return gcd(b, a % b) if b != 0 else a
 
+def extended_gcd(a: int, b: int) -> tuple:
+    assert a >= 0 and b >= 0
+    # compute d, x, y
+    if b == 0:
+        d, x, y = a, 1, 0
+    else:
+        d, p, q = extended_gcd(b, a % b)
+        x = q
+        y = p - (a // b) * q
+    # check answers
+    assert d == a * x + b * y
+    return (d, x, y)
+
 if __name__ == "__main__":
-    d = gcd(24, 16)
-    print(d)
+    res = extended_gcd(24, 7)
+    print(res)
